74c21cb8ab7d32c0f4adf1245900cb2d000d6d1b
priyanka <bedarkar.priyanka95@gmail.com>
2019-11-24 17:48:12 -0500

    adding custom login page

--- Voting/src/main/java/com/voting/web/RootController.java
+++ Voting/src/main/java/com/voting/web/RootController.java
@@ -1,13 +1,15 @@
-package com.voting;
+package com.voting.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class DashboardController {
+public class RootController {
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	//@RequestMapping(value="/", method=RequestMethod.GET)
+	@GetMapping("/")
 	public String rootView() {
 		return "index";
 	}

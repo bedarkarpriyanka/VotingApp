71d6553f815875cf09ef309d257ca5e54721829f
priyanka <bedarkar.priyanka95@gmail.com>
2019-11-26 21:43:31 -0500

    adding product entity

--- Voting/src/main/java/com/voting/domain/User.java
+++ Voting/src/main/java/com/voting/domain/User.java
@@ -22,6 +22,7 @@ public class User {
 	private String password;
 	private String name;
 	private Set<Authority> authorities = new HashSet<>();
+	private Set<Product> products = new HashSet<>();
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -61,6 +62,14 @@ public class User {
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", name=" + name
 				+ ", authorities=" + authorities + "]";
+	}
+	
+	@OneToMany(cascade=CascadeType.PERSIST, fetch=FetchType.LAZY, mappedBy="user")
+	public Set<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(Set<Product> products) {
+		this.products = products;
 	}
 	
 }
